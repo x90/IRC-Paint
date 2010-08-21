732af6e1c55bdc06d9590464d584499c606965ca
Pedro <Pedrobear9001@gmail.com>
2010-08-21 19:20:19 -0300

    derp; also, addendum to the last commit message: fuck azure

--- MainWidget.cpp
+++ MainWidget.cpp
@@ -2,8 +2,6 @@
 
 #include "MainWidget.h"
 
-#include "IRCPaintMainWindow.h"
-
 MainWidget::MainWidget(QWidget *parent, QMap<int, QRgb>* colors) : QWidget(parent) {
     setAttribute(Qt::WA_StaticContents);
     setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
