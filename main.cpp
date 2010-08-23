a38d9c90d5c388ce8afbabfe381448d771d88d45
Pedro <Pedrobear9001@gmail.com>
2010-08-23 19:54:10 -0300

    add a todo list  ÎÎÎ=ÎÎÎ=ÎÎÎ=ÎÎÎÎ´ÎÎ`ÎÎÎÎ (ÎÎÎÎÎÎÎÎ

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 
 #include <QtGui/QApplication>
 
+/* *
+   * TODO:
+   * * Actually implement the user interface
+   * * Split the image importing code into its own dialog (finer selection of conversion methods and whatnot)
+   * * Implement settings tracking (and a preferences dialog)
+   * * Add more brushes (namely selection brush and fill brush)
+   * * Backport ascii.pl transformations (and make it work with selection ranges)
+   * * Implement undo/redo
+   * * Export as an image (both raw size and a larger size)
+   * * Implement clipboard
+   * * Use signals and slots where applicable for communication between the main window, widgets and brushes
+   * * Implement scrollbars in the main window
+   * * Make sure irc paint doesn't lag like a fat bitch when working with large asciis
+   *
+   * */
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     IRCPaintMainWindow w;
