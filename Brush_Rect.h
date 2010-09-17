5c97dcfc5ee07f4cc3ad9da7e77ca4fd928d5d06
Pedro <Pedrobear9001@gmail.com>
2010-09-17 18:43:47 -0300

    sup gcc

--- Brush_Rect.h
+++ Brush_Rect.h
@@ -12,7 +12,7 @@ class Brush_Rect : public Brush {
     bool drawPreview, drawOnBg;
     QColor col;
 public:
-    Brush_Rect(MainWidget* w) : Brush(w), drawPreview(false), drawOnBg(false), xstart(0), ystart(0), xend(0), yend(0) {}
+    Brush_Rect(MainWidget* w) : Brush(w), drawPreview(false), xstart(0), drawOnBg(false), ystart(0), xend(0), yend(0) {}
     bool onMouseClick(QMouseEvent *event, int x, int y, bool insideWidget);
     bool onMouseMove(QMouseEvent *event, int x, int y, bool insideWidget);
     bool onMouseRelease(QMouseEvent *event, int x, int y, bool insideWidget);
